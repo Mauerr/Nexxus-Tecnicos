b10fdba3e01ede10ef47aff376ec0bc545be0149
Nick L. Ruiz <nickl.ruiz.contacto@gmail.com>
2025-11-19 16:38:21 -0600

    test de camara en fisico Android

--- lib/src/presentation/pages/auth/administrador/validaciondevehiculosadmin.dart
+++ lib/src/presentation/pages/auth/administrador/validaciondevehiculosadmin.dart
@@ -3,6 +3,7 @@ import 'package:nexxus/src/presentation/pages/auth/administrador/documentacionAd
 import 'package:nexxus/src/presentation/pages/auth/administrador/evidenciafotografica.dart';
 import 'package:nexxus/src/presentation/pages/auth/administrador/homeadmin.dart';
 import 'package:nexxus/src/presentation/pages/auth/administrador/opcionesvalidacionvehiculo.dart';
+import 'package:nexxus/src/services/camara_service.dart';
 
 class ValidacionVehiculosAdmin extends StatefulWidget {
   const ValidacionVehiculosAdmin({super.key});
@@ -95,18 +96,21 @@ class _ValidacionVehiculosAdminState extends State<ValidacionVehiculosAdmin> {
 
                   // Botones principales
                   // Botones principales
-                  customButton(
-                    context,
-                    "Evidencia Fotográfica",
-                    () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const EvidenciafotograficaScreen(),
-                        ),
-                      );
-                    },
-                  ),
+                  ElevatedButton(
+  onPressed: () async {
+    final foto = await CamaraService.tomarFoto();
+
+    if (foto != null) {
+      print("Foto tomada: ${foto.path}");
+      // Aquí puedes:
+      // - Guardar en BLoC
+      // - Subir al backend
+      // - Mostrar preview
+    }
+  },
+  child: const Text("Abrir cámara"),
+),
+
                   const SizedBox(height: 20),
 
                   customButton(
